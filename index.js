06b3d9b5966cb2d34cf38fbf58860ff33597da6e
Jasper Concepcion <onetruemanok@gmail.com>
2021-02-17 05:32:08 +0800

    sample listener for user + room

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const socketIO = require("socket.io");
 const INDEXFILE = "/index.html";
+const { userJoin, getCurrentUser } = require("./utils/users");
 
 const port = process.env.PORT || 3200;
 const server = express()
@@ -14,6 +15,14 @@ const io = socketIO(server);
 io.on("connection", (socket) => {
   console.log("User joined => ", socket.id);
   socket.emit("message", "Welcome my gamer");
+
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(user.room);
+    socket
+      .to(user.room)
+      .emit("message", `What's gucci my gamer? @${user.username}`);
+  });
 });
 
 // setInterval(() => io.emit("time", new Date().toTimeString()), 1000 * 60);
