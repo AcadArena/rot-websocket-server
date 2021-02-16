827a469d06c035fa2f16669e4e507325c0c95555
Jasper Concepcion <onetruemanok@gmail.com>
2021-02-16 20:42:23 +0800

    added sample event

--- index.js
+++ index.js
@@ -11,4 +11,9 @@ const server = express()
 
 const io = socketIO(server);
 
+io.on("connection", (socket) => {
+  console.log("User joined => ", socket.id);
+  socket.emit("message", "Welcome my gamer");
+});
+
 // setInterval(() => io.emit("time", new Date().toTimeString()), 1000 * 60);
